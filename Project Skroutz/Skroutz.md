da559dcc98af3580e9dddd0252ff36124f4b1945
StathisKan <stevekanab@hotmail.com>
2017-06-11 20:20:06 +0300

    Add files via upload

---
+++ Project Skroutz/Skroutz.md
@@ -0,0 +1,36 @@
+# Project Skroutz
+
+Our aim is to develop a website similar to skroutz.gr (or scrooge.co.uk) limited to *technology* products.
+
+Development of the website should start from an empty MVC project and will be separated into phases of advancing difficulty and complexity.
+
+## Skroutz - Phase A
+In Phase A, we only have **products**, **categories** and **stores**. Categories can have hierarchy, a product has only one category, and a store can sell many products and a product can be sold in many stores with different price.  
+1. Create the model.  
+2. Populate the database with seed data (c/p from original website if you like).  
+3. Implement a Data Access Layer (DAL) exposing methods for common database actions.  
+3. Create controllers and views for viewing/inserting/editing/deleting the model.  
+
+Categories:
+* Computers  
+  * Hardware
+  * Monitors
+* Entertainment
+  * Televisions & Accessories
+  * Projectors & Accessories
+* Gaming
+  * Consoles
+  * Virtual Reality
+
+## Skroutz - Phase B
+Every product has attributes. Products of the same category have the same attribute definitions, but different attribute values.
+1. Extend the model.  
+2. Populate the database (not all attributes are needed, but at least those that scrooge.co.uk uses for filtering).  
+3. Update the controllers and views.  
+
+## Skroutz - Phase C
+After inserting enough entries to the model, continue with the design of the main website. It should be similar to original website:  
+1. Home page with full-screen section for search, boxes of categories.  
+2. Each category has its own page with boxes of subcategories.  
+3. Categories with no children lead to list of products. (No filters for now)  
+4. Page of product displaying details, list of stores etc.  
---
+++ Project Skroutz/Skroutz/Skroutz/Controllers/InventoriesController.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Skroutz.DAL;
+using Skroutz.Models;
+
+namespace Skroutz.Controllers
+{
+    public class InventoriesController : Controller
+    {
+        private SkroutzContext db = new SkroutzContext();
+
+        // GET: Inventories
+        public async Task<ActionResult> Index()
+        {
+            var inventories = db.Inventories.Include(i => i.Product).Include(i => i.Store);
+            return View(await inventories.ToListAsync());
+        }
+
+        // GET: Inventories/Details/5
+        public async Task<ActionResult> Details(int? productId, int? storeId)
+        {
+            if (productId == null || storeId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = await db.Inventories.FindAsync(productId, storeId);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // GET: Inventories/Create
+        public ActionResult Create()
+        {
+            ViewBag.ProductId = new SelectList(db.Products.Include(x => x.Category).ToList(), "Id", "Name", "Category.Name", 0);
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name");
+            return View();
+        }
+
+        // POST: Inventories/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Create([Bind(Include = "ProductId,StoreId,Price")] Inventory inventory)
+        {
+            var inv = db.Inventories.Find(inventory.ProductId, inventory.StoreId);
+            if (ModelState.IsValid && inv == null)
+            {
+                db.Inventories.Add(inventory);
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.ProductId = new SelectList(db.Products.OrderBy(x => x.Name), "Id", "Name", "Category.Name", inventory.ProductId);
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name", inventory.StoreId);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Edit/5
+        public async Task<ActionResult> Edit(int? productId, int? storeId)
+        {
+            if (productId == null || storeId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = await db.Inventories.FindAsync(productId, storeId);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.ProductId = new SelectList(db.Products.OrderBy(x => x.Name), "Id", "Name", "Category.Name", inventory.ProductId);
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name", inventory.StoreId);
+            return View(inventory);
+        }
+
+        // POST: Inventories/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Edit([Bind(Include = "ProductId,StoreId,Price")] Inventory inventory)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(inventory).State = EntityState.Modified;
+                await db.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ProductId = new SelectList(db.Products.OrderBy(x => x.Name), "Id", "Name", "Category.Name", inventory.ProductId);
+            ViewBag.StoreId = new SelectList(db.Stores, "Id", "Name", inventory.StoreId);
+            return View(inventory);
+        }
+
+        // GET: Inventories/Delete/5
+        public async Task<ActionResult> Delete(int? productId, int? storeId)
+        {
+            if (productId == null || storeId == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Inventory inventory = await db.Inventories.FindAsync(productId, storeId);
+            if (inventory == null)
+            {
+                return HttpNotFound();
+            }
+            return View(inventory);
+        }
+
+        // POST: Inventories/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> DeleteConfirmed(int productId, int storeId)
+        {
+            Inventory inventory = await db.Inventories.FindAsync(productId, storeId);
+            db.Inventories.Remove(inventory);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
